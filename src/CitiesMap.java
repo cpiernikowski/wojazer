e0a6c0657c5d4a5937a248a2cc56abfe05abd846
cpiernikowski <czarson2115@wp.pl>
2024-06-03 22:08:35 +0200

    Graph -> CitiesMap

--- src/CitiesMap.java
+++ src/CitiesMap.java
@@ -2,14 +2,12 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.HashMap;
 
-public class Graph {
-    private HashMap<Pair<Integer>, Double> edges;
+public class CitiesMap {
+    private double[][] mat;
     private int n_vertices;
 
-    Graph(File f) throws IOException {
-        this.edges = new HashMap<>();
+    CitiesMap(File f) throws IOException {
         BufferedReader reader = new BufferedReader(new FileReader(f));
         this.n_vertices = 0;
         while (reader.readLine() != null)
@@ -17,14 +15,15 @@ public class Graph {
 
         reader = new BufferedReader(new FileReader(f));
 
+        mat = new double[this.n_vertices][this.n_vertices];
+
         int row = 0;
         while (reader.ready()) {
             String line = reader.readLine();
             String[] vals = line.split(" ");
             assert vals.length == this.n_vertices; // zaklada, ze dane sa w poprawnym formacie, po stronie uzytkownika programu stoi zweryfikowanie ich poprawnosci
             for (int col = 0; col < this.n_vertices; ++col) {
-                edges.put(new Pair<>(row + 1, col + 1), Double.valueOf(vals[col]));
-                edges.put(new Pair<>(col + 1, row + 1), Double.valueOf(vals[col]));
+                mat[col][row] = Double.valueOf(vals[col]);
             }
             ++row;
         }
@@ -32,17 +31,12 @@ public class Graph {
         reader.close();
     }
 
-    public HashMap<Pair<Integer>, Double> get_edges() {
-        return this.edges;
+    public double[][] get_mat() {
+        return this.mat;
     }
-
     public double distance_between(int a, int b) {
         assert a <= this.n_vertices && b <= this.n_vertices;
-        return edges.get(new Pair<Integer>(a, b));
-    }
-
-    public double distance_between(Pair<Integer> p) {
-        return distance_between(p.x, p.y);
+        return this.mat[a][b];
     }
 
     public int n_of_vertices() {
