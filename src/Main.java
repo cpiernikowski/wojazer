14b51c857d9f90e1b6081e9bb283935e2ca46d0f
cpiernikowski <czarson2115@wp.pl>
2024-06-03 23:45:17 +0200

    main CitiesMap

--- src/Main.java
+++ src/Main.java
@@ -4,18 +4,18 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        Graph g = new Graph(new File("in.txt"));
+        CitiesMap cm = new CitiesMap(new File("in.txt"));
 
-        int[] ch = GeneticAlgorithm.random_tsp_solution(g.n_of_vertices());
+        int[] ch = GeneticAlgorithm.random_tsp_solution(cm.n_of_vertices());
 
         System.out.println(Arrays.toString(ch));
-        double d_sztywno = g.distance_between(ch[0], ch[1])
-                           + g.distance_between(ch[1], ch[2])
-                           + g.distance_between(ch[2], ch[3])
-                           + g.distance_between(ch[3], ch[4])
-                           + g.distance_between(ch[4], ch[0]);
+        double d_sztywno = cm.distance_between(ch[0], ch[1])
+                           + cm.distance_between(ch[1], ch[2])
+                           + cm.distance_between(ch[2], ch[3])
+                           + cm.distance_between(ch[3], ch[4])
+                           + cm.distance_between(ch[4], ch[0]);
 
-        double d_fn = GeneticAlgorithm.chromosome_fitness(ch, g);
+        double d_fn = GeneticAlgorithm.chromosome_fitness(ch, cm);
 
         assert d_sztywno == d_fn;
 
